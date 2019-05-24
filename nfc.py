89d4155ed32b1be481d1f8662e3698a6855f4fe0
esuau <evan.suau@etu.u-pec.fr>
2019-05-24 12:34:08 +0200

    refactor(read): Define main function

--- nfc.py
+++ nfc.py
@@ -2,17 +2,23 @@ import time
 
 import nxppy
 
-mifare = nxppy.Mifare()
 
-while True:
-    try:
-        uid = mifare.select()
-        print(uid)
-        ndef_data = mifare.ndef_data()
-        print(ndef_data)
-    except nxppy.SelectError:
-        pass
-    except MemoryError:
-        print('Could not read chip')
-        pass
-    time.sleep(1)
+def main():
+    mifare = nxppy.Mifare()
+
+    while True:
+        try:
+            uid = mifare.select()
+            print(uid)
+            ndef_data = mifare.ndef_data()
+            print(ndef_data)
+        except nxppy.SelectError:
+            pass
+        except MemoryError:
+            print('Could not read chip')
+            pass
+        time.sleep(1)
+
+
+if __name__ == '__main__':
+    main()
