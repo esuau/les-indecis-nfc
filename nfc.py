9ab407584e1809af05c272c96a46d415d990fe19
esuau <evan.suau@etu.u-pec.fr>
2019-05-24 12:40:52 +0200

    refactor(read): Parse data from NDEF JSON text

--- nfc.py
+++ nfc.py
@@ -1,3 +1,5 @@
+# coding=utf-8
+import json
 import time
 
 import nxppy
@@ -11,11 +13,15 @@ def main():
             uid = mifare.select()
             print(uid)
             ndef_data = mifare.ndef_data()
-            print(ndef_data)
+            data = json.loads(ndef_data[5:])
+            print(data)
         except nxppy.SelectError:
             pass
         except MemoryError:
             print('Could not read chip')
+            pass
+        except ValueError:
+            print('Could not parse data from JSON')
             pass
         time.sleep(1)
 
